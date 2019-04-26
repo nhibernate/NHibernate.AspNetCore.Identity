196a52859da8a401317a462bd3a7c41e4e780cb1
Chun Liang <huahchunliang@hotmail.com>
2019-04-26 11:16:02 +0800

    Update ApplicationUsers and ApplicationRoles
    - Update 01_application_users.sql remove CustomProperty and add CreateTime, LastLogin, LoginCount. (Follow ApplicationUser.hbm.xml mapping)
    - Update 01_application_roles.sql remove CustomProperty and add Description. (Follow ApplicationRole.hbm.xml mapping).
    - Update ApplicationUser.hbm.xml and ApplicationRole.hbm.xml.

--- test/WebTest/Entities/ApplicationRole.cs
+++ test/WebTest/Entities/ApplicationRole.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using NHIdentityRole = NHibernate.AspNetCore.Identity.IdentityRole;
+﻿using NHIdentityRole = NHibernate.AspNetCore.Identity.IdentityRole;
 
 namespace WebTest.Entities {
-    
     public class ApplicationRole : NHIdentityRole {
-
-        public virtual string CustomProperty { get; set; }
-
+        public virtual string Description { get; set; }
     }
-
 }
