19ede9b2b64e7ffd1d039d30fa8f0be1c16e79ca
beginor <beginor@qq.com>
2021-10-13 10:57:49 +0800

    fix push sh.

--- push-package.sh
+++ push-package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 dotnet pack src/NHibernate.AspNetCore.Identity/NHibernate.AspNetCore.Identity.csproj -c Release
-dotnet nuget push src/NHibernate.AspNetCore.Identity/bin/Release/NHibernate.AspNetCore.Identity.5.0.14.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
+dotnet nuget push src/NHibernate.AspNetCore.Identity/bin/Release/NHibernate.AspNetCore.Identity.5.0.15.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
 rm src/NHibernate.AspNetCore.Identity/bin/Release/*.nupkg
