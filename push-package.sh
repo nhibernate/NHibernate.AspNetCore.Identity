821e74b8954a11fdf9675a02906bda6ad94616aa
beginor <beginor@qq.com>
2022-08-11 14:17:05 +0800

    Update to dotnet 6.0.8 .

--- push-package.sh
+++ push-package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 dotnet pack src/NHibernate.AspNetCore.Identity/NHibernate.AspNetCore.Identity.csproj -c Release
-dotnet nuget push src/NHibernate.AspNetCore.Identity/bin/Release/NHibernate.AspNetCore.Identity.6.0.9.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
+dotnet nuget push src/NHibernate.AspNetCore.Identity/bin/Release/NHibernate.AspNetCore.Identity.6.0.10.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
 rm src/NHibernate.AspNetCore.Identity/bin/Release/*.nupkg
