96d70e29aebcea649823ece143b21fe072c2cb6a
beginor <beginor@qq.com>
2022-12-20 11:20:20 +0800

    update package version

--- push-package.sh
+++ push-package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+PACKAGE_VERSION="7.0.1"
 dotnet pack src/NHibernate.AspNetCore.Identity/NHibernate.AspNetCore.Identity.csproj -c Release
-dotnet nuget push src/NHibernate.AspNetCore.Identity/bin/Release/NHibernate.AspNetCore.Identity.7.0.0.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
+dotnet nuget push src/NHibernate.AspNetCore.Identity/bin/Release/NHibernate.AspNetCore.Identity.$PACKAGE_VERSION.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
 rm src/NHibernate.AspNetCore.Identity/bin/Release/*.nupkg
