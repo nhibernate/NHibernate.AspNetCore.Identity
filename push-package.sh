c65dfd18744a44b85c1915ff2232603a5eec9b75
beginor <beginor@qq.com>
2025-08-18 14:38:17 +0800

    refactor to use env vars.

--- push-package.sh
+++ push-package.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
-export PACKAGE_VERSION="9.0.4"
-dotnet pack src/NHibernate.AspNetCore.Identity/NHibernate.AspNetCore.Identity.csproj -c Release
-dotnet nuget push src/NHibernate.AspNetCore.Identity/bin/Release/NHibernate.AspNetCore.Identity.$PACKAGE_VERSION.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
-rm src/NHibernate.AspNetCore.Identity/bin/Release/*.nupkg
+export PACKAGE_VERSION="9.0.5"
+export PACKAGE_RELEASE_NOTES="Update to .NET 9.0.8;Update Identity.Core to 9.0.8;"
+export PACKAGE_TAGS="nhibernate, aspnetcore, identity"
+
+PROJECTS=( \
+  "src/NHibernate.AspNetCore.Identity/NHibernate.AspNetCore.Identity.csproj" \
+)
+
+for PROJ in "${PROJECTS[@]}"
+do
+  echo "packing $PROJ"
+  dotnet pack $PROJ -c Release --output ./nupkgs/
+done
+
+dotnet nuget push ./nupkgs/*.nupkg -s nuget.org -k $(cat ~/.nuget/github.txt) \
+  --skip-duplicate
+
+rm -rf ./nupkgs
