2c72880770ba74ce62381dd94aaf7cfae8a5d81c
beginor <beginor@qq.com>
2025-12-27 12:33:11 +0800

    refactor to use extension block

--- src/NHibernate.AspNetCore.Identity/ConfigurationExtensions.cs
+++ src/NHibernate.AspNetCore.Identity/ConfigurationExtensions.cs
@@ -5,57 +5,54 @@ namespace NHibernate.AspNetCore.Identity;
 
 public static class ConfigurationExtensions {
 
-    public static Configuration AddIdentityMappings(this Configuration cfg) {
-        var dialect = cfg.GetProperty(NHibernate.Cfg.Environment.Dialect);
-        if (dialect.IndexOf("PostgreSQL", StringComparison.OrdinalIgnoreCase) > -1 || dialect.IndexOf("Npgsql", StringComparison.OrdinalIgnoreCase) > -1) {
-            cfg.AddIdentityMappingsForPostgres();
+    extension(Configuration cfg) {
+
+        public Configuration AddIdentityMappings() {
+            var dialect = cfg.GetProperty(NHibernate.Cfg.Environment.Dialect);
+            if (dialect.IndexOf("PostgreSQL", StringComparison.OrdinalIgnoreCase) > -1 || dialect.IndexOf("Npgsql", StringComparison.OrdinalIgnoreCase) > -1) {
+                cfg.AddIdentityMappingsForPostgres();
+            }
+            else if (dialect.IndexOf("MySQL", StringComparison.OrdinalIgnoreCase) > -1) {
+                cfg.AddIdentityMappingsForMySql();
+            }
+            else if (dialect.IndexOf("MsSql", StringComparison.OrdinalIgnoreCase) > -1) {
+                cfg.AddIdentityMappingsForMsSql();
+            }
+            else if (dialect.IndexOf("Sqlite", StringComparison.OrdinalIgnoreCase) > -1) {
+                cfg.AddIdentityMappingsForSqlite();
+            }
+            else {
+                throw new NotSupportedException(
+                    "Only support PostgreSQL, MsSql, MySQL, Sqlite, your dialect must contain one of these words!"
+                );
+            }
+            return cfg;
         }
-        else if (dialect.IndexOf("MySQL", StringComparison.OrdinalIgnoreCase) > -1) {
-            cfg.AddIdentityMappingsForMySql();
+
+        public Configuration AddIdentityMappingsForPostgres() {
+            var mapping = ConfigurationHelper.GetIdentityMappingForPostgreSql();
+            cfg.AddXml(mapping.AsString());
+            return cfg;
         }
-        else if (dialect.IndexOf("MsSql", StringComparison.OrdinalIgnoreCase) > -1) {
-            cfg.AddIdentityMappingsForMsSql();
+
+        public Configuration AddIdentityMappingsForMsSql() {
+            var mapping = ConfigurationHelper.GetIdentityMappingForMsSql();
+            cfg.AddXml(mapping.AsString());
+            return cfg;
         }
-        else if (dialect.IndexOf("Sqlite", StringComparison.OrdinalIgnoreCase) > -1) {
-            cfg.AddIdentityMappingsForSqlite();
+
+        public Configuration AddIdentityMappingsForMySql() {
+            var mapping = ConfigurationHelper.GetIdentityMappingForMySql();
+            cfg.AddXml(mapping.AsString());
+            return cfg;
         }
-        else {
-            throw new NotSupportedException(
-                "Only support PostgreSQL, MsSql, MySQL, Sqlite, your dialect must contain one of these words!"
-            );
+
+        public Configuration AddIdentityMappingsForSqlite() {
+            var mapping = ConfigurationHelper.GetIdentityMappingForSqlite();
+            cfg.AddXml(mapping.AsString());
+            return cfg;
         }
-        return cfg;
+
     }
 
-    public static Configuration AddIdentityMappingsForPostgres(
-        this Configuration cfg
-    ) {
-        var mapping = ConfigurationHelper.GetIdentityMappingForPostgreSql();
-        cfg.AddXml(mapping.AsString());
-        return cfg;
-    }
-
-    public static Configuration AddIdentityMappingsForMsSql(
-        this Configuration cfg
-    ) {
-        var mapping = ConfigurationHelper.GetIdentityMappingForMsSql();
-        cfg.AddXml(mapping.AsString());
-        return cfg;
-    }
-
-    public static Configuration AddIdentityMappingsForMySql(
-        this Configuration cfg
-    ) {
-        var mapping = ConfigurationHelper.GetIdentityMappingForMySql();
-        cfg.AddXml(mapping.AsString());
-        return cfg;
-    }
-
-    public static Configuration AddIdentityMappingsForSqlite(
-        this Configuration cfg
-    ) {
-        var mapping = ConfigurationHelper.GetIdentityMappingForSqlite();
-        cfg.AddXml(mapping.AsString());
-        return cfg;
-    }
 }
